b14687d8146fcd11d235143d3b6c1479924fb6ed
David L. Inman <davidlevere@gmail.com>
2021-08-07 00:31:42 -0400

    whitespace rule and fix up identifier

--- src/grammar.rs
+++ src/grammar.rs
@@ -1,19 +1,28 @@
 
 use std::collections::HashMap;
-use rparse::{ParseRule, Data, self};
+use rparse::{ParseRule, Data, Field, self};
 
+fn whitespace(rules : &mut HashMap<String, ParseRule>) {
+    rules.insert("whitespace".to_string()
+                , zero_or_more!( on!( |c| c.is_whitespace() ) )
+                );
+}
 
 fn identifier(rules : &mut HashMap<String, ParseRule>) {
     rules.insert( "identifier".to_string()
-                , and!{ on!(|c| c.is_alphabetic() || c == '_')
-                      , zero_or_more!( on!( |c| c.is_alphanumeric() || c == '_' ) )
-                      } );
+                , and! { invoke_rule!( "whitespace" ) 
+                       , and!{ on!(|c| c.is_alphabetic() || c == '_')
+                             , zero_or_more!( on!( |c| c.is_alphanumeric() || c == '_' ) )
+                             } 
+                       , invoke_rule!( "whitespace" )
+                       }
+                );
 }
-
 
 pub fn parse(input : &str) -> Result<Data, ()> {
     let mut rules = HashMap::new();
 
+    whitespace(&mut rules);
     identifier(&mut rules);
 
     rparse::parse("main", &rules, input)
