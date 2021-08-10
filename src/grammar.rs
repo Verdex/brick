16e390b11ff60f9f0faf79c37a4b8c357c6f33ec
David L. Inman <davidlevere@gmail.com>
2021-08-09 22:36:31 -0400

    number literal parser and test

--- src/grammar.rs
+++ src/grammar.rs
@@ -19,11 +19,29 @@ fn identifier(rules : &mut HashMap<String, ParseRule>) {
                 );
 }
 
+fn string_literal(rules : &mut HashMap<String, ParseRule>) {
+
+}
+
+fn bool_literal(rules : &mut HashMap<String, ParseRule>) {
+
+}
+
+fn number_literal(rules : &mut HashMap<String, ParseRule>) {
+    rules.insert( "number_literal".to_string()
+                , and! { invoke_rule!( "whitespace" ) 
+                       , one_or_more!( on!(|c| c.is_numeric()) )
+                       , invoke_rule!( "whitespace" )
+                       }
+                );
+}
+
 pub fn parse(input : &str) -> Result<Data, ()> {
     let mut rules = HashMap::new();
 
     whitespace(&mut rules);
     identifier(&mut rules);
+    number_literal(&mut rules);
 
     rparse::parse("main", &rules, input)
 }
@@ -32,17 +50,18 @@ pub fn parse(input : &str) -> Result<Data, ()> {
 mod test {
     use super::*;
 
-    fn get_ids(data : &Data) -> Vec<String> {
-        fn next_data(data : &Data) -> Vec<&Data> {
-            match data {
-                Data::Nil => vec![],
-                Data::Char(_) => vec![],
-                Data::Field { data, .. } => vec![&data],
-                Data::List(list) => {
-                    list.iter().collect::<Vec<&Data>>()
-                },
-            }
+    fn next_data(data : &Data) -> Vec<&Data> {
+        match data {
+            Data::Nil => vec![],
+            Data::Char(_) => vec![],
+            Data::Field { data, .. } => vec![&data],
+            Data::List(list) => {
+                list.iter().collect::<Vec<&Data>>()
+            },
         }
+    }
+
+    fn get_ids(data : &Data) -> Vec<String> {
 
         let find : fn(&Data) -> Vec<&Data> = all_matches!(next_data, Data::List(_), Data::Field { .. }, Data::List(_), Data::List(_));
         let find_first : fn(&Data) -> Vec<&Data> = all_matches!(next_data, Data::List(_), Data::Char(_));
@@ -60,6 +79,48 @@ mod test {
                 _ => panic!("expected only Data::Char"),
             }).collect()
         }).collect()
+    }
+
+    fn get_numbers(data : &Data) -> Vec<u32> {
+        let find : fn(&Data) -> Vec<&Data> = all_matches!( next_data
+                                                         , Data::List(_)
+                                                         , Data::Field { .. }
+                                                         , Data::List(_)
+                                                         , Data::List(_)
+                                                         );
+        find(&data).iter().map(|number| {
+            let digits = match number {
+                Data::List(digits) => digits,
+                _ => panic!("expected list"),
+            };
+
+            digits.iter().map(|digit| match digit {
+                Data::Char(d) => d,
+                _ => panic!("expected char"),
+            }).collect::<String>().parse::<u32>().unwrap()
+        }).collect::<Vec<u32>>()
+    }
+
+    #[test]
+    fn should_parse_numbers() -> Result<(), ()> {
+        let mut rules = HashMap::new();
+
+        whitespace(&mut rules);
+        number_literal(&mut rules);
+
+        rules.insert("main".to_string(), one_or_more!( invoke_rule!("number_literal") ) );
+
+        let result = rparse::parse("main", &rules, "1 1234 09876")?;
+
+        let ids = get_numbers(&result);
+
+        assert_eq!( ids.len(), 3 );
+
+        assert_eq!( ids[0], 1 );
+        assert_eq!( ids[1], 1234 );
+        assert_eq!( ids[2], 9876 );
+
+        Ok(())
     }
 
     #[test]
